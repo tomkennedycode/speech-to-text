372ce9b94fa8a0e05e83226c4964ebb6b4dac2c6
Tom Kennedy <tom.kennedy017@gmail.com>
2020-07-21 17:13:20 +0100

    renamed poorly named variables

--- speech-to-text.py
+++ speech-to-text.py
@@ -1,15 +1,15 @@
-import speech_recognition as sr
+import speech_recognition as recognition
 import pyaudio
 import wave
 
-p = pyaudio.PyAudio()
+audio = pyaudio.PyAudio()
 
 def getAllInputDevices():
-    info = p.get_host_api_info_by_index(0)
-    numdevices = info.get('deviceCount')
-    for i in range(0, numdevices):
-        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            print('Device id ', i, ' - ', p.get_device_info_by_host_api_device_index(0, i).get('name'))
+    info = audio.get_host_api_info_by_index(0)
+    numberOfDevices = info.get('deviceCount')
+    for i in range(0, numberOfDevices):
+        if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print('Device id ', i, ' - ', audio.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 def recordSnippet():
     CHUNK = 1024
@@ -20,7 +20,7 @@ def recordSnippet():
     device_index = 2
     WAVE_OUTPUT_FILENAME = "output.wav"
 
-    stream = p.open(format=FORMAT,
+    stream = audio.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
                     input=True,
@@ -39,27 +39,27 @@ def recordSnippet():
 
     stream.stop_stream()
     stream.close()
-    p.terminate()
+    audio.terminate()
 
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+    create = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    create.setnchannels(CHANNELS)
+    create.setsampwidth(audio.get_sample_size(FORMAT))
+    create.setframerate(RATE)
+    create.writeframes(b''.join(frames))
+    create.close()
 
 def speechToText():
-    r = sr.Recognizer()
+    recogniser = recognition.Recognizer()
 
     #Read axe wav file
-    with sr.AudioFile('output.wav') as source:
+    with recognition.AudioFile('output.wav') as source:
         
-        audio_text = r.listen(source)
+        audio_text = recogniser.listen(source)
         
     # using google speech recognition
         try:
             
-            text = r.recognize_google(audio_text)
+            text = recogniser.recognize_google(audio_text)
             print('Converting audio transcripts into text ...')
             print(text)
         
